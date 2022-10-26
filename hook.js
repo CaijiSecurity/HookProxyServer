8c228f7e9bc9f33ee32db0dbc5277816e60cc07f
unknown <boringboys@fakeemail.com>
2022-10-27 00:36:00 +0800

    hook.js更新20221026

--- hook.js
+++ hook.js
@@ -2,53 +2,73 @@
 
 console.log("当前的函数有")
 
-function hookAllFunction(caijiObj, objStr) {
-    // debugger;
-    window.Object.keys(caijiObj).forEach(
-        (elem, index) => {
-            // console.log(index, elem, typeof (eval(elem)))
-            if (typeof (eval(objStr + elem)) == "function") {
-                console.log("函数", elem);
-                var hookIt = "var _oldFunc = " + objStr + elem + ";" +
-                    objStr + elem + " = function(){" +
-                    "console.log('开始执行', " + objStr + elem + ");" +
-                    "console.log('参数：', arguments);" +
-                    "let result = _oldFunc.apply(this, arguments);" +
-                    "console.log('执行结束', " + objStr + elem + ");" +
-                    "console.log('结果：', result);" +
-                    "return result;" +
-                    "}";
-                eval(hookIt);
-                console.log(objStr + elem, "已被hook!");
-            } else if (typeof (eval(objStr + elem)) == "object" && eval(objStr + elem)) {
-                if (eval(objStr + elem) != caijiObj) {
-                    console.log("开始遍历对象", objStr + elem, "的子对象");
-                    hookAllFunction(eval(objStr + elem), objStr + elem + ".");
-                }
-            }
-        }
-    )
+function isNative(fn) {
+	// return (/\{\s*\[native code\]\s*\}/).test('' + fn);
+    // return eval(fn + ".toString().indexOf('[native code]') != -1");
+    return false;
 }
 
-hookAllFunction(window, "window.")
+var doNotHookArray = [
+    '__core-js_shared__',
+    'requestAnimationFrame',
+    'clearInterval',
+    'setInterval'
+]
 
-// (function() {
-//     // 保存原始方法
-//     window.__cr_fun = window.Function;
-//     // 重写 function
-//     var myfun = function() {
-//         var args = Array.prototype.slice.call(arguments, 0, -1).join(","),
-//             src = arguments[arguments.length - 1];
-//         console.log(src);
-//         console.log("=============== Function end ===============");
-//         debugger;
-//         return window.__cr_fun.apply(this, arguments);
-//     }
-//     // 屏蔽js中对原生函数native属性的检测
-//     myfun.toString = function() {
-//         return window.__cr_fun + ""
-//     }
-//     Object.defineProperty(window, 'Function', {
-//         value: myfun
-//     });
-// })();
+function hookAllFunction(caijiObj, objStr, flag) {
+    // debugger;
+    if (window.Object.keys(caijiObj).length){
+        window.Object.keys(caijiObj).forEach(
+            (elem, index) => {
+                // console.log(index, elem, typeof (eval(elem)))
+                // console.log(objStr, elem);
+                // if (doNotHookArray.indexOf(elem) == -1) {
+                if (elem.indexOf('-') == -1 && elem.indexOf('@') == -1 && doNotHookArray.indexOf(elem) == -1) {
+                    if (typeof (eval(objStr + elem)) == "function") {
+                        if (isNative(objStr + elem)) {
+                            // console.log(objStr + elem, "()是原生func，不hook");
+                        } else if (objStr + elem == 'window.onload') {
+                            if (flag == 1){
+                                console.log("函数", elem, "hook该函数，在页面加载完成时再hook一次！");
+                                var hookIt = "var _oldFunc = " + objStr + elem + ";" +
+                                    objStr + elem + " = function(){" +
+                                    "hookAllFunction(window, 'window.', 2);" +
+                                    "console.log('开始执行', " + objStr + elem + ");" +
+                                    "console.log('参数：', arguments);" +
+                                    "let result = _oldFunc.apply(this, arguments);" +
+                                    "console.log('执行结束', " + objStr + elem + ");" +
+                                    "console.log('结果：', result);" +
+                                    "return result;" +
+                                    "}";
+                                eval(hookIt);
+                                console.log(objStr + elem, "已被hook!");
+                            }
+                        } else {
+                            if (flag == 2){
+                                console.log("函数", elem);
+                                var hookIt = "var _oldFunc = " + objStr + elem + ";" +
+                                    objStr + elem + " = function(){" +
+                                    "console.log('开始执行', " + objStr + elem + ");" +
+                                    "console.log('参数：', arguments);" +
+                                    "let result = _oldFunc.apply(this, arguments);" +
+                                    "console.log('执行结束', " + objStr + elem + ");" +
+                                    "console.log('结果：', result);" +
+                                    "return result;" +
+                                    "}";
+                                eval(hookIt);
+                                console.log(objStr + elem, "已被hook!");
+                            }
+                        }
+                    } else if (typeof (eval(objStr + elem)) == "object" && eval(objStr + elem) && !eval(objStr + elem + ' instanceof Array')) {
+                        if (eval(objStr + elem) != caijiObj) {
+                            // console.log("开始遍历对象", objStr + elem, "的子对象");
+                            hookAllFunction(eval(objStr + elem), objStr + elem + ".");
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
+
+hookAllFunction(window, "window.", 1)
